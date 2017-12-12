2901a9d673885f75c4f973ac66a306014c36617e
fh543567 <fh543567@fh-muenster.de>
2017-12-12 10:46:59 +0100

    Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    [MatButtonModule, MatCheckboxModule],
   ],
   providers: [],
   bootstrap: [AppComponent]
