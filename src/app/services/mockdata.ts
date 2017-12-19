93518156e5f4aab3428df3636b357e585906cc0c
fh543567 <fh543567@fh-muenster.de>
2017-12-19 12:10:02 +0100

    mockdata befüllt um Mock Daten bereitzustellen

--- src/app/services/mockdata.ts
+++ src/app/services/mockdata.ts
@@ -3,13 +3,39 @@ import { Story } from '../story/story';
 import { Epic } from '../epic/epic';
 
 export const TASK: Task[] = [
-  {  }
+  {
+    id: 1,
+    name: 'Task 1',
+    description: 'Beschreibung task1',
+    priority: 3,
+    estimatedTime: 10,
+    workedTime: 0
+  },
+  {
+    id: 2,
+    name: 'Task 2',
+    description: 'Beschreibung task2',
+    priority: 2,
+    estimatedTime: 20,
+    workedTime: 2
+  }
 ];
-
 export const STORY: Story[] = [
-  {  }
+  {
+    id: 1,
+    name: 'Story 1',
+    description: 'Beschreibung Story 1',
+    priority: 1,
+    tasks: TASK
+  }
 ];
 
 export const EPIC: Epic[] = [
-  {  }
+  {
+    id: 1,
+    name: 'Epic 1',
+    description: 'Beschreibung Epic 1',
+    priority: 1,
+    stories: STORY
+  }
 ];
