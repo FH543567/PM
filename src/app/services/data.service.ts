e271b92133fd542fae37ef6d38d9fa747d5b717a
fh543567 <fh543567@fh-muenster.de>
2017-12-19 12:52:49 +0100

    Mockdaten über Service bereitstellen (Ohne Observable)

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { EPIC, TASK, STORY } from './mockdata';
+import { Task } from '../task/task';
+import { Story } from '../story/story';
+import { Epic } from '../epic/epic';
 
 @Injectable()
 export class DataService {
 
   constructor() { }
 
+  getTasks(): Task[] {
+    return TASK;
+  }
+
+  getStory(): Story[] {
+    return STORY;
+  }
+
+  getEpic(): Epic[] {
+    return EPIC;
+  }
 }
