8eb6d40c0b6de59ffc5b6839645e29fffecf4024
unknown <malde.e@web.de>
2018-01-22 19:02:34 +0100

    epics.service erstellt

--- src/app/services/epic.service.ts
+++ src/app/services/epic.service.ts
@@ -2,45 +2,55 @@ import { Injectable } from '@angular/core';
 import { Epic } from '../epic/epic';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
-import { DataService } from './data.service';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/throw';
+import { DtoService } from './dto.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
-export class EpicService {
+export class EpicService extends DtoService {
 
-  constructor() { }
+  constructor(http: HttpClient) {
+    super('http://localhost:3000/api/epics', http);
+  }
 
   getAll(): Observable<Epic[]> {
-    return of(EPICS);
+    return super.getAll()
+      .map(epicList => epicList = epicList
+        .map(epicDB => epicDB = new Epic(epicDB.EpicID, epicDB.Name, epicDB.Description, epicDB.Priority)));
   }
 
   getById(id: number): Observable<Epic> {
-    for (let i = 0; i < EPICS.length; i++) {
-      if (EPICS[i].id === id) {
-        return of(EPICS[i]);
-      }
-    }
-    return null;
+    return super.getById(id)
+      .map(epicDB => epicDB = new Epic(epicDB.EpicID, epicDB.Name, epicDB.Description, epicDB.Priority))
+        //.catch(e => { console.log(e); return undefined; })
   }
 
-  create(epic: Epic): boolean {
-    console.log('created');
-    console.log('Name: ' + epic.name);
-    console.log('Description: ' + epic.description);
-    console.log('Priority: ' + epic.priority);
-    return true;
+  create(epic): Observable<Epic> {
+    var transferObject: any = {};
+    //ID wird nicht berücksichtigt, da auto-increment
+    //transferObject.EpicId = epic.id;
+    transferObject.Name = epic.name;
+    transferObject.Description = epic.description;
+    transferObject.Priority = epic.priority;
+    console.log(JSON.stringify(transferObject));
+    return super.create(JSON.stringify(transferObject));;
   }
 
-  update(epic: Epic): boolean {
-    console.log('updated');
-    return true;
+  update(epic): Observable<Epic> {
+    var transferObject: any = {};
+    transferObject.EpicID = epic.id;
+    transferObject.Name = epic.name;
+    transferObject.Description = epic.description;
+    transferObject.Priority = epic.priority;
+    console.log(JSON.stringify(transferObject));
+    return super.update(JSON.stringify(transferObject));;
   }
 
-  delete(id: number): boolean {
-    console.log('deleted');
-    return true;
+  delete(id: number): Observable<number> {
+    return super.delete(id);
   }
+
 
 }
