480441ae4fa651f000828b10eee58fa84b0e3d98
Malte <Sipsip@users.noreply.github.com>
2018-01-23 11:12:40 +0100

    Sprint service angepasst

--- src/app/services/sprint.service.ts
+++ src/app/services/sprint.service.ts
@@ -1,43 +1,86 @@
 import { Injectable } from '@angular/core';
-import { SPRINTS } from './mockdata';
 import { Sprint } from '../sprint/sprint';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/throw';
+import { DtoService } from './dto.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
-export class SprintService {
+export class SprintService extends DtoService {
 
-  constructor() { }
+  constructor(http: HttpClient) {
+    super('http://localhost:3000/api/sprints', http);
+  }
 
+  /**
+   * Alle Sprints auf der DB abfragen
+   * @returns {Observable<Sprint[]>} 
+   */
   getAll(): Observable<Sprint[]> {
-    return of(SPRINTS);
+    return super.getAll()
+      .map(sprintList => sprintList = sprintList
+        .map(sprintDB => sprintDB = new Sprint(sprintDB.SprintID, sprintDB.Name, sprintDB.Description, sprintDB.AvailableTime, sprintDB.StartDate, sprintDB.EndDate)));
   }
 
+  /**
+   * Ein Sprint anhand der ID abfragen
+   * @returns {Observable<Sprint>} returnt 'undefined' wenn id nicht gefunden wurde
+   * @param id ID des gesuchten Sprints
+   */
   getById(id: number): Observable<Sprint> {
-    for (let i = 0; i < SPRINTS.length; i++) {
-      if (SPRINTS[i].id === id) {
-        return of(SPRINTS[i]);
-      }
-    }
-    return null;
+    return super.getById(id)
+      .map(sprintDB => sprintDB = new Sprint(sprintDB.SprintID, sprintDB.Name, sprintDB.Description, sprintDB.AvailableTime, sprintDB.StartDate, sprintDB.EndDate))
+        //.catch(e => { console.log(e); return undefined; })
   }
 
-  getAllSorted(): Observable<Sprint[]> {
-    return null;
+  /**
+   * Sprint erstellen
+   * Sprint wird ohne ID an den Server geschickt (ID wird einfach ignoriert)
+   * @param {resource} Sprint
+   * @returns {Observable<Sprint>}
+   */
+  create(sprint): Observable<Sprint> {
+    var transferObject: any = {};
+    //ID wird nicht berücksichtigt, da auto-increment
+    //transferObject.SprintId = sprint.id;
+    transferObject.Name = sprint.name;
+    transferObject.Description = sprint.description;
+    transferObject.AvailableTime = sprint.availableTime;
+    transferObject.StartDate = sprint.startDate;
+    transferObject.EndDate = sprint.endDate;
+    console.log(JSON.stringify(transferObject));
+    return super.create(JSON.stringify(transferObject));;
   }
 
-  create(sprint: Sprint) {
-    console.log('Create Sprint');
-    console.log('Name: ' + sprint.name);
-    console.log('Description: ' + sprint.description);
-    console.log('Available time: ' + sprint.availableTime);
-    console.log('Start date: ' + sprint.startDate);
-    console.log('End date: ' + sprint.endDate);
+  /**
+   * Sprint bearbeiten
+   * Das Sprint in der DB mit der gleichen ID wird durch das gesendete Sprint ersetzt.
+   * Existiert kein Sprint mit dieser ID in der DB, wird ein neues erstellt.
+   * @param {resource} Sprint
+   * @returns {Observable<Sprint>}
+   */
+  update(sprint): Observable<Sprint> {
+    var transferObject: any = {};
+    transferObject.SprintID = sprint.id;
+    transferObject.Name = sprint.name;
+    transferObject.Description = sprint.description;
+    transferObject.AvailableTime = sprint.availableTime;
+    transferObject.StartDate = sprint.startDate;
+    transferObject.EndDate = sprint.endDate;
+    console.log(JSON.stringify(transferObject));
+    return super.update(JSON.stringify(transferObject));;
   }
 
-  update(sprint: Sprint) {
+  /**
+   * Ein Sprint anhand der ID löschen
+   * @returns {Observable<number>} 
+   * @param id ID des zu löschenden Sprints
+   */
+  delete(id: number): Observable<number> {
+    return super.delete(id);
   }
 
-  delete(id: number) {
-  }
 }
