2dfef6c80a1a1f9536b359e6af43cd24df91fea6
unknown <malde.e@web.de>
2018-01-25 16:09:11 +0100

    subscribe von Scrumboad-page angepasst

--- src/app/scrumboard-page/scrumboard-page.component.ts
+++ src/app/scrumboard-page/scrumboard-page.component.ts
@@ -3,6 +3,9 @@ import { Story } from '../story/story';
 import { Task } from '../task/task';
 import { StoryService } from '../services/story.service';
 import { TaskService } from '../services/task.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/observable/zip';
+import 'rxjs/observable/merge';
 
 @Component({
   selector: 'app-scrumboard-page',
@@ -18,19 +21,21 @@ export class ScrumboardPageComponent implements OnInit {
   constructor(private storyService: StoryService, private taskService: TaskService) { }
 
   ngOnInit() {
-    this.getStories();
-    this.getTasks();
-    this.sortTasks();
+    this.getStories().subscribe(stories => this.stories = stories);
+    //sortTasks() wird erst ausgeführt, wenn die Antwort von getTasks() angekommen ist:
+    this.getTasks().subscribe(tasks => {
+      this.tasks = tasks, 
+      this.sortTasks()
+    });
+    
   }
 
-  getStories() {
-    this.storyService.getAll()
-      .subscribe(stories => this.stories = stories);
+  getStories() : Observable<Story[]> {
+    return this.storyService.getAll()
   }
 
-  getTasks() {
-      this.taskService.getAll()
-        .subscribe(tasks => this.tasks = tasks);
+  getTasks() : Observable<Task[]> {
+      return this.taskService.getAll()
   }
 
   sortTasks() {
